54c0d74ed4dbf1a39e4e944011bd55cb0286e9b5
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-04 11:10:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const config = {
   challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    admin: "balls",
+    guests: "67",
+    banjo: "monke",
   },
 };
 
